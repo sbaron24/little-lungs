9ee7794ac7450b044c4e405145c4187d892f9d01
Sean B <xseany@gmail.com>
2024-11-13 18:35:44 -0500

    test change

--- app/components/ParkSearchBar.js
+++ app/components/ParkSearchBar.js
@@ -1,5 +1,5 @@
 const ParkSearchBar = () => {
-  return <div>Park Search Bar (goes here)</div>;
+  return <div>Park Search Bar (goes here SOME CHANGE GOES HERE)</div>;
 };
 
 export default ParkSearchBar;
